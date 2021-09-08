0dd798cffbf56d8ddc29dfb2613844c509008a9b
Chandani-Gurung <gurungchandani13@gmail.com>
2021-09-08 14:03:26 -0500

    some minor updates

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import ReactApp from '../ReactApp';
+
+function Home() {
+    return (
+        <>
+            <ReactApp />
+        </>
+    )
+};
+
+
+export default Home
