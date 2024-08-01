651cce0d6a4c29cbd80bdf2c6fdba998ed6f81f1
Chandani-Gurung <gurungchandani13@gmail.com>
2024-08-01 16:18:56 -0500

    minor update

--- src/components/ReactApp.js
+++ src/components/ReactApp.js
@@ -7,7 +7,7 @@ function ReactApp() {
             <div className="site-title">
             <h1>Chandani Gurung</h1>
             </div>
-            <p class="site-description">Junior Software Developer</p>
+            <p class="site-description">Junior Software Engineer</p>
             <img src="/images/image1.jpg" />
         </div>
     )
